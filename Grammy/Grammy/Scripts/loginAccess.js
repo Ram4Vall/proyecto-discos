a4a0ce1fc1b91f0e48ad45849f1265de39bc2cb3
Wyrna <m.jesus.patricio@hotmail.com>
2017-02-20 12:47:10 +0100

    Modificar Login

---
+++ Grammy/Grammy/Scripts/loginAccess.js
@@ -0,0 +1,36 @@
+﻿function searchClient() {
+   
+    $.ajax({
+        url: '../api/Clientes',
+        type: "GET",
+        dataType: "json",
+        contentType: "application/json",
+        success: function (Clientes) {
+            $.each(Clientes, function (index) {
+                if (Clientes[index].Username == $('#username').val() && Clientes[index].Password == $('#password').val()) {
+                    //Volver a la pagina principal, cambiar Div "Bienvenido 'nombre'
+                    var obClient = JSON.stringify(Clientes[index]);
+                    localStorage.setItem('ClienteLogeado', obClient);
+                }
+            })
+        }
+    });
+}
+
+function searchClient() {
+    var client = { Email: $("#email1").val(), Username: $("#email1").val(), Password: $("#password1").val() };
+    $.ajax({
+        type: "POST",
+        data: JSON.stringify(client),
+        url: "../api/Clientes",
+        contentType: "application/json",
+        success: function (Clientes) {
+            $.each(Clientes, function (index) {
+                console.log(Clientes[index]);
+            }
+            )
+        }
+
+
+    });
+}
